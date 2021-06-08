120bb76edeaf0a576a4de51cc28199c6df381fa9
btgurss <82287707+btgurss@users.noreply.github.com>
2021-06-08 15:34:16 -0500

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -39,9 +39,9 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
 
 CREATE TABLE department_employees(
 	emp_no INT NOT NULL,
@@ -53,4 +53,4 @@ CREATE TABLE department_employees(
     PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM titles;
